be947a2410eefdef79355a28965e6f03caba4784
Nalivayko Ivan <nalivayko.rus@gmail.com>
2022-07-15 18:10:33 +0300

    (android), местоположение, остановка/старт сессии

--- android/src/main/java/ru/inaliv/appmetrica/AppMetrica.java
+++ android/src/main/java/ru/inaliv/appmetrica/AppMetrica.java
@@ -1,6 +1,7 @@
 package ru.inaliv.appmetrica;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 
 import com.yandex.metrica.YandexMetrica;
@@ -12,9 +13,15 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import org.json.JSONException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @NativePlugin
 public class AppMetrica extends Plugin {
     private final Object mLock = new Object();
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
     private boolean mActivityPaused = true;
     private boolean mAppMetricaActivated = false;
 
@@ -24,7 +31,7 @@ public class AppMetrica extends Plugin {
      * @param config
      * @return
      */
-    public static YandexMetricaConfig toConfig(JSObject config) {
+    public static YandexMetricaConfig toConfig(final JSObject config) throws JSONException {
         final String apiKey = config.getString("apiKey");
         final YandexMetricaConfig.Builder builder = YandexMetricaConfig.newConfigBuilder(apiKey);
 
@@ -47,23 +54,46 @@ public class AppMetrica extends Plugin {
             builder.withLogs();
         }
         if (config.has("location")) {
-//            final Location location = toLocation(configObj.getJSONObject("location"));
-//            builder.withLocation(location);
+            final Location location = toLocation(config.getJSObject("location"));
+            builder.withLocation(location);
         }
 
         return builder.build();
     }
 
+    /**
+     * Конвертирует конфигурацию JSObject в объект Location для App Метрики
+     *
+     * @param location
+     * @return
+     * @throws JSONException
+     */
+    public static Location toLocation(final JSObject location) throws JSONException {
+        final Location yandexLocation = new Location("Custom");
 
-    @PluginMethod
-    public void init(PluginCall call) {
-        final String apiKey = call.getString("key");
-        final Context context = this.bridge.getActivity().getApplicationContext();
+        if (location.has("latitude")) {
+            yandexLocation.setLatitude(location.getDouble("latitude"));
+        }
+        if (location.has("longitude")) {
+            yandexLocation.setLongitude(location.getDouble("longitude"));
+        }
+        if (location.has("altitude")) {
+            yandexLocation.setAltitude(location.getDouble("altitude"));
+        }
+        if (location.has("accuracy")) {
+            yandexLocation.setAccuracy((float) location.getDouble("accuracy"));
+        }
+        if (location.has("course")) {
+            yandexLocation.setBearing((float) location.getDouble("course"));
+        }
+        if (location.has("speed")) {
+            yandexLocation.setSpeed((float) location.getDouble("speed"));
+        }
+        if (location.has("timestamp")) {
+            yandexLocation.setTime(location.getLong("timestamp"));
+        }
 
-        YandexMetricaConfig config = YandexMetricaConfig.newConfigBuilder(apiKey).withLogs().build();
-        YandexMetrica.activate(context, config);
-
-        call.success();
+        return yandexLocation;
     }
 
     /**
@@ -73,8 +103,14 @@ public class AppMetrica extends Plugin {
      */
     @PluginMethod
     private void activate(PluginCall call) {
-        final YandexMetricaConfig config = toConfig(call.getData());
-        final Context context = bridge.getActivity().getApplicationContext();
+        final YandexMetricaConfig config;
+        try {
+            config = toConfig(call.getData());
+        } catch (JSONException e) {
+            call.error("Не удалось активировать метрику: " + e.getMessage());
+            return;
+        }
+        final Context context = getBridge().getActivity().getApplicationContext();
 
         YandexMetrica.activate(context, config);
 
@@ -130,5 +166,86 @@ public class AppMetrica extends Plugin {
         call.success();
     }
 
+    /**
+     * Задать объект Location для метрики
+     * @param call
+     * @throws JSONException
+     */
+    @PluginMethod
+    private void setLocation(PluginCall call) {
+        final JSObject locationObj = call.getData();
 
+        try {
+            final Location location = toLocation(locationObj);
+            YandexMetrica.setLocation(location);
+            call.success();
+        } catch (JSONException e) {
+            call.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Отслеживание местоположения (вкл/выкл)
+     * @param call
+     */
+    private void setLocationTracking(PluginCall call) {
+        final boolean enabled = call.getBoolean("enabled");
+        YandexMetrica.setLocationTracking(enabled);
+        call.success();
+    }
+
+    /**
+     * Возобновить сессию
+     */
+    private void onResumeSession() {
+        synchronized (mLock) {
+            mActivityPaused = false;
+            if (mAppMetricaActivated) {
+                YandexMetrica.resumeSession(getActivity());
+            }
+        }
+    }
+
+    /**
+     * Поставить на паузу сессию
+     */
+    private void onPauseSession() {
+        synchronized (mLock) {
+            mActivityPaused = true;
+            if (mAppMetricaActivated) {
+                YandexMetrica.pauseSession(getActivity());
+            }
+        }
+    }
+
+    /**
+     * Handle onNewIntent
+     * @param intent
+     */
+    protected void handleOnNewIntent(Intent intent) {
+        mExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (mAppMetricaActivated) {
+                    YandexMetrica.reportAppOpen(getBridge().getActivity());
+                }
+            }
+        });
+    }
+
+    /**
+     * Handle onResume
+     */
+    @Override
+    protected void handleOnResume() {
+        onResumeSession();
+    }
+
+    /**
+     * Handle onPause
+     */
+    @Override
+    protected void handleOnPause() {
+        onPauseSession();
+    }
 }
