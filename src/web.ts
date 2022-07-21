f5b196f61a06196586f5469a02ac4ca16a47e93f
Nalivayko Ivan <nalivayko.rus@gmail.com>
2022-07-21 15:22:11 +0300

    (web) Заглушка для веб-реализации

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,16 @@
 import { WebPlugin } from '@capacitor/core';
-import { AppMetricaPlugin } from './definitions';
+import {
+  AppMetricaPlugin,
+  ECommerceCartItem,
+  ECommerceOrder,
+  ECommerceScreen,
+  YAMConfig,
+  YAMLocation,
+  YAMReportErrorOptions,
+  YAMReportEventOptions,
+  YAMShowProductCardEventOptions, 
+ YAMShowProductDetailsEventOptions
+} from './definitions';
 
 export class AppMetricaWeb extends WebPlugin implements AppMetricaPlugin {
   constructor() {
@@ -8,10 +19,41 @@ export class AppMetricaWeb extends WebPlugin implements AppMetricaPlugin {
       platforms: ['web'],
     });
   }
-
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  activate(config: YAMConfig): Promise<{ activated: boolean; }> {
+    return Promise.reject('Web not supported. [activate()]');
+  }
+  reportEvent(options: YAMReportEventOptions): Promise<void> {
+    return Promise.reject('Web not supported. [reportEvent()]');
+  }
+  reportError(options: YAMReportErrorOptions): Promise<void> {
+    return Promise.reject('Web not supported. [reportError()]');
+  }
+  setLocation(location: YAMLocation): Promise<void> {
+    return Promise.reject('Web not supported. [setLocation()]');
+  }
+  setLocationTracking(options: { enabled: boolean; }): Promise<void> {
+    return Promise.reject('Web not supported. [setLocationTracking()]');
+  }
+  showScreenEvent(screen: ECommerceScreen): Promise<void> {
+    return Promise.reject('Web not supported. [showScreenEvent()]');
+  }
+  showProductCardEvent(options: YAMShowProductCardEventOptions): Promise<void> {
+    return Promise.reject('Web not supported. [showProductCardEvent()]');
+  }
+  showProductDetailsEvent(options: YAMShowProductDetailsEventOptions): Promise<void> {
+    return Promise.reject('Web not supported. [showProductDetailsEvent()]');
+  }
+  addCartItemEvent(cartItem: ECommerceCartItem): Promise<void> {
+    return Promise.reject('Web not supported. [addCartItemEvent()]');
+  }
+  removeCartItemEvent(cartItem: ECommerceCartItem): Promise<void> {
+    return Promise.reject('Web not supported. [removeCartItemEvent()]');
+  }
+  beginCheckoutEvent(order: ECommerceOrder): Promise<void> {
+    return Promise.reject('Web not supported. [beginCheckoutEvent()]');
+  }
+  purchaseEvent(order: ECommerceOrder): Promise<void> {
+    return Promise.reject('Web not supported. [purchaseEvent()]');
   }
 }
 
